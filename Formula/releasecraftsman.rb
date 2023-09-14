ba363f39d63dd2632539d3f537f7cf9a392ba30d
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2023-09-15 01:30:16 +0400

    Update releasecraftsman.rb

--- Formula/releasecraftsman.rb
+++ Formula/releasecraftsman.rb
@@ -1,9 +1,9 @@
 class Releasecraftsman < Formula
   desc "CLI tool for parsing commit logs and generating beautiful release notes"
   homepage "https://github.com/tornikegomareli/release-craftsman"
-  url "https://github.com/tornikegomareli/release-craftsman/releases/download/0.1.0/releasecraftsman-0.1.0-x86_64-apple-darwin-tar.gz"
-  sha256 "3d65eabfad5a1cb9fd11f16f06d6f8fd430841c837b2f6edb33ed67272939c46"
-  version "0.1.0"
+  url "https://github.com/tornikegomareli/release-craftsman/releases/download/0.1.1/releasecraftsman-0.1.1-x86_64-apple-darwin-tar.gz"
+  sha256 "8cdec501485dd461d64dd0d29c5e2e61b062398c60a2f8c5181a77e7b4b9c08c"
+  version "0.1.1"
 
   def install
     bin.install "releasecraftsman"
